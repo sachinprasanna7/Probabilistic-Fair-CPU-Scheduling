d4d798089b39032f6017da82dd07b84875410772
Sachin <sachincodes1@gmail.com>
2023-05-11 12:34:48 +0530

    changed main file, added a tempmain file which has round robin code for comparsion

--- tempmain.cpp
+++ tempmain.cpp
@@ -8,6 +8,7 @@
 #include <cstdio>
 using namespace std;
 
+void RR(int processes[][2], int n, int time_quantum);
 
 double calculateProbability(int priority, int totalPriority, int n, int k) {
 
@@ -173,7 +174,7 @@ void probabilisticFair(int processes[][3], int n, int time_quantum){
 
 
     printf("\n\nProcess ID\t| Priority\t| Arrival Time\t| Burst Time\t| Completion Time\t| Turnaround Time\t| Waiting Time");
-    
+    //print the same thing as above using cout
 
     for (int i = 0; i < n; i++){
         int turnaaroundTime = completionTime[i] - arrivalTime[i];
@@ -190,15 +191,128 @@ void probabilisticFair(int processes[][3], int n, int time_quantum){
 
 }
 
+
+
 int main(){
 
     int arr[][3] = {{0, 1, 3}, {1, 2, 6}, {2, 3, 7}, {3, 7, 2}, {4, 8, 8}, {5, 9, 9}, {6, 14, 5}, {7, 15, 4}, {8, 16, 1}};
     int n = sizeof(arr)/sizeof(arr[0]);
     int time_quantum = 2;
 
-    for(int i = 0 ; i < 10 ; i++){
-        probabilisticFair(arr, n, time_quantum);
-        cout<<endl;
-    }
+    //int arrRR[][2] = {{0,3}, {0,5}, {3,2}, {16, 4}};
+
+    probabilisticFair(arr, n, time_quantum);
+    cout<<endl;
+    probabilisticFair(arr, n, time_quantum);
+    cout<<endl;
+    probabilisticFair(arr, n, time_quantum);
+    cout<<endl;
+    probabilisticFair(arr, n, time_quantum);
+    cout<<endl;
+    probabilisticFair(arr, n, time_quantum);
+    cout<<endl;
+    probabilisticFair(arr, n, time_quantum);
+    cout<<endl;
+    probabilisticFair(arr, n, time_quantum);
+    cout<<endl;
+    probabilisticFair(arr, n, time_quantum);
+    cout<<endl;
+    probabilisticFair(arr, n, time_quantum);
+    cout<<endl;
+    probabilisticFair(arr, n, time_quantum);
+
+    //RR(arrRR, n, time_quantum);
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// void RR(int processes[][2], int n, int time_quantum)
+// {
+//     int p[100], arrivalTime[100], burstTime[100], burstTimeFinal[100], completionTime[100], turnaaroundTime[100], waitingTime[100], vis[100] = {0}, i, j, temp = 0, pr[100], m, bf[10000];
+//     float avgWaitingTime = 0, avgTurnaaroundTime = 0;
+
+//     int min = 100000000, ix = -1;
+//     for (i = 0; i < n; i++)
+//     {
+//         p[i] = i + 1;
+//         arrivalTime[i] = processes[i][0];
+//         burstTime[i] = processes[i][1];
+//         burstTimeFinal[i] = burstTime[i];
+//         if (min > arrivalTime[i])
+//         {
+//             min = arrivalTime[i];
+//             ix = i;
+//         }
+//     }
+
+//     int ans = 0, ii = 0, jj = 0;
+//     bf[jj++] = ix;
+//     vis[ix] = 1;
+//     for (j = 0; j < n;)
+//     {
+//         printf("%d ", bf[ii] + 1);
+//         int o = burstTime[bf[ii]];
+//         if (o > time_quantum)
+//         {
+//             ans += time_quantum;
+//             burstTime[bf[ii]] -= time_quantum;
+//             for (i = 0; i < n; i++)
+//             {
+//                 if (vis[i] == 0 && arrivalTime[i] <= ans)
+//                 {
+//                     bf[jj++] = i;
+//                     vis[i] = 1;
+//                 }
+//             }
+//             bf[jj++] = bf[ii];
+//         }
+//         else
+//         {
+//             ans += o;
+//             burstTime[bf[ii]] -= o;
+//             for (i = 0; i < n; i++)
+//             {
+//                 if (vis[i] == 0 && arrivalTime[i] <= ans)
+//                 {
+//                     bf[jj++] = i;
+//                     vis[i] = 1;
+//                 }
+//             }
+//             completionTime[bf[ii]] = ans;
+//             j++;
+//         }
+
+//         ii++;
+//     }
+
+//     printf("\n\nProcess\t| Arrival Time\t| Burst Time\t| Completion Time\t| Turnaround Time\t| Waiting Time");
+//     for (i = 0; i < n; i++)
+//     {
+//         turnaaroundTime[i] = completionTime[i] - arrivalTime[i];
+//         waitingTime[i] = turnaaroundTime[i] - burstTimeFinal[i];
+//         avgTurnaaroundTime += turnaaroundTime[i];
+//         avgWaitingTime += waitingTime[i];
+//     }
+
+//     avgTurnaaroundTime = avgTurnaaroundTime / n;
+//     avgWaitingTime = avgWaitingTime / n;
+
+//     for (i = 0; i < n; i++)
+//     {
+//         printf("\n------------------------------------------------------------------------------------------------------");
+//         printf("\nP%d\t| %d\t\t| %d\t\t| %d\t\t\t| %d\t\t\t| %d", p[i], arrivalTime[i], burstTimeFinal[i], completionTime[i], turnaaroundTime[i], waitingTime[i]);
+//     }
+//     printf("\n------------------------------------------------------------------------------------------------------");
+//     printf("\nAverage Turnaround Time: %.2f", avgTurnaaroundTime);
+//     printf("\nAverage Waiting Time: %.2f", avgWaitingTime);
+// }
